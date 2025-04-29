4966987e078084d7e7ea980444324b94590d6780
kishlaykiku <kishlaykumarkkp24@gmail.com>
2025-04-30 00:24:24 +0530

    Init

---
+++ Array/BuySellStock.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class BuySellStock {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    // Solution
+    public int maxProfit(int[] prices) {
+
+        int minBuyPrice = prices[0];
+        int profit = 0;
+        int maxProfit = 0;
+
+        for(int i = 0; i < prices.length-1; i++) {
+
+            // Track minimum price of stock
+            if(prices[i] < minBuyPrice)
+                minBuyPrice = prices[i];
+
+            profit = prices[i+1] - minBuyPrice;
+
+            // Track maximum profit generated
+            if(profit > maxProfit)
+                maxProfit = profit;
+        }
+
+        return maxProfit;
+    }
+
+    // For CPH
+    public static void main(String args[]) {
+
+        BuySellStock bss = new BuySellStock();
+        ArrayList<Integer> arr = new ArrayList<>();
+        int price = 0;
+
+        while(sc.hasNext()) {
+
+            if(sc.hasNextInt()) {
+                price = sc.nextInt();
+                arr.add(price);
+            }
+            else {
+                String input = sc.next();
+                if(input.equalsIgnoreCase("q")) // When input is "q", loop will stop
+                    break;
+            }
+        }
+        sc.close();
+
+        // Convert from ArrayList to Array
+        int[] prices = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++) {
+            prices[i] = arr.get(i);
+        }
+
+        int output = bss.maxProfit(prices);
+        System.out.println(output);
+    }
+}
