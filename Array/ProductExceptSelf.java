853e4592c239e5e292b83945df4f98b7ca580438
kishlaykiku <kishlaykumarkkp24@gmail.com>
2025-05-05 11:25:12 +0530

    ProductExceptSelf

--- Array/ProductExceptSelf.java
+++ Array/ProductExceptSelf.java
@@ -11,13 +11,20 @@ public class ProductExceptSelf {
         int[] productArray = new int[nums.length];
 
         // Optimized [O(n)]
-        int left = 1;
-        int right  = 1;
+        int[] left = new int[nums.length];
+        int[] right  = new int [nums.length];
 
-        for(int i = 0; i < nums.length; i++) {
+        left[0] = 1;
+        right[nums.length-1] = 1;
 
-            
-        }
+        for(int i = 1; i < nums.length; i++)
+            left[i] = left[i-1] * nums[i-1];
+
+        for(int i = nums.length-2; i >= 0; i--)
+            right[i] = right[i+1] * nums[i+1];
+
+        for(int i = 0; i < nums.length; i++)
+            productArray[i] = left[i] * right[i];
 
         // Brute Force
         // int product = 1;
