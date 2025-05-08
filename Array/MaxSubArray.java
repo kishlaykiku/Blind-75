0758f6271870f9901f00686dc273e9349ea62d21
kishlaykiku <kishlaykumarkkp24@gmail.com>
2025-05-08 12:11:41 +0530

    MaxSubArray

---
+++ Array/MaxSubArray.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class MaxSubArray {
+
+    private static final Scanner sc = new Scanner(System.in);
+
+    // Solution
+    public int solution(int[] nums) {
+
+        int maxSum = Integer.MIN_VALUE;
+        int currentSum = 0;
+
+        // Optimized[Kadane's Algoriothm - O(n)]
+        for(int i = 0; i < nums.length; i++) {
+
+            currentSum += nums[i];
+            maxSum = Math.max(maxSum, currentSum);
+
+            if(currentSum < 0)
+                currentSum = 0;
+        }
+
+        // Brute Force
+        // for(int i = 0; i < nums.length; i++) {
+
+        //     currentSum = 0;
+
+        //     for(int j = i; j < nums.length; j++) {
+
+        //         currentSum += nums[j];
+        //         maxSum = Math.max(maxSum, currentSum);
+        //     }
+        // }
+
+        return maxSum;
+    }
+
+    // For CPH
+    public static void main(String args[]) {
+
+        MaxSubArray msa = new MaxSubArray();
+        ArrayList<Integer> arr = new ArrayList<>();
+        int number = 0;
+
+        while(sc.hasNext()) {
+            if(sc.hasNextInt()) {
+                number = sc.nextInt();
+                arr.add(number);
+            }
+            else {
+                String input = sc.next();
+                if(input.equalsIgnoreCase("q")) // When input is "q", loop will stop
+                    break;
+            }
+        }
+        sc.close();
+
+        // Convert from ArrayList to Array
+        int[] nums = new int[arr.size()];
+        for(int i = 0; i < arr.size(); i++)
+            nums[i] = arr.get(i);
+
+        // Output
+        int output = msa.solution(nums);
+        System.out.print(output);
+    }
+}
